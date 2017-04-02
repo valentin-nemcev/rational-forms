d27fb23a2207836b2dc4af71c9a4a01e966429cd
Валентин Немцев <mail@valentin-nemcev.com>
2017-04-02 23:23:34 +0300

    feat: Add action items

--- src/Excercise.js
+++ src/Excercise.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import Radium from 'radium'
 
+const actionStyle = {
+  fontStyle: 'italic'
+}
+
 class Excercise extends Component {
   render () {
     return (
       <div>
         <h2>{this.props.title}</h2>
+        <p style={actionStyle}>
+          {this.props.action}
+        </p>
         {this.props.children}
       </div>
     )
