48187ddc9719bd6584de9aedf5bc437173d624f9
Валентин Немцев <mail@valentin-nemcev.com>
2017-04-09 20:30:50 +0300

    refactor(Make Excercise accept props instead of children):

--- src/Excercise.js
+++ src/Excercise.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import Radium from 'radium'
 
+import { connect } from 'react-redux'
+
+import Textarea from './Textarea'
+
 const actionStyle = {
   fontStyle: 'italic'
 }
 
+const Answer = connect(({example}) => ({example}))(({example, children}) =>
+  <div>
+    <h3>Упражнение</h3>
+    {children}
+    Ваш пример<br />
+    {example}
+    <Textarea />
+  </div>
+)
 export default Radium((props) =>
   <div>
     <h2>{props.title}</h2>
     <p style={actionStyle}>
       {props.action}
     </p>
-    {props.children}
+    {props.explanation}
+    <Answer>{props.instruction}</Answer>
   </div>
 )
